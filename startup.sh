565495cba1f5bd8425167f68ada1f2203243f44d
Bobbejak <jonathangreyj5.com>
2025-02-18 23:52:50 +0900

    Updated docker setup with startup script

--- startup.sh
+++ startup.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# Wait for MariaDB to be ready
+until mysqladmin ping -h"$DRUPAL_DB_HOST" --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 5
+done
+
 echo "🚀 Checking for module updates..."
 composer install --no-interaction --prefer-dist
 
-echo "🚀 Importing latest Drupal configuration..."
+echo "📥 Importing latest Drupal configuration..."
 drush config-import -y
 
-echo "🚀 Clearing cache..."
+echo "🧹 Clearing cache..."
 drush cr
 
 echo "✅ Startup checks completed."
+
+# Keep container running with Apache
+exec apache2-foreground
