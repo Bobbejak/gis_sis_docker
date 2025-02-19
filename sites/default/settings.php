efff311a8b492142ce7d419e39002100a70608b3
Bobbejak <jonathan@greyj5.com>
2025-02-19 11:17:42 +0900

    Reverted to pre-installation state and fixed settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,14 +1,15 @@
 <?php
 
 $databases['default']['default'] = array (
-  'database' => getenv('DRUPAL_DATABASE_NAME') ?: 'gis_sis',
-  'username' => getenv('DRUPAL_DATABASE_USER') ?: 'gis_sis_user',
-'password' => getenv('DRUPAL_DATABASE_PASSWORD') ?: 'user_240885MP',
-  'host' => getenv('DRUPAL_DATABASE_HOST') ?: 'db',
+  'database' => 'gis_sis',
+  'username' => 'gis_sis_user',
+  'password' => 'user_240885MP',
+  'host' => 'db',
   'port' => '3306',
   'driver' => 'mysql',
   'prefix' => '',
 );
+
 
   
 
@@ -16,8 +17,9 @@ $databases['default']['default'] = array (
 $settings['hash_salt'] = 'random-generated-string';
 
 // Allow file system writes
-$settings['file_private_path'] = '../private';
-$settings['config_sync_directory'] = '../config/sync';
+$settings['file_private_path'] = '/var/www/html/sites/default/private';
+$settings['config_sync_directory'] = '/var/www/html/config/sync';
+
 $settings['update_free_access'] = FALSE;
 
 // Ensure correct permissions
