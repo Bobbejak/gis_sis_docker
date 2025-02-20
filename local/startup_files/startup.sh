ba6af5abe5b274dc7e34bb8e1f341fdcab64db9b
Bobbejak <jonathan@greyj5.com>
2025-02-20 16:45:14 +0900

    Fine tuned apache, php, mysql (Maria) for performance improvement

--- local/startup_files/startup.sh
+++ local/startup_files/startup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+# Convert CRLF to LF if needed
+if file "$0" | grep -q CRLF; then
+    echo "Fixing line endings for startup.sh..."
+    sed -i 's/\r$//' "$0"
+    chmod +x "$0"  # Ensure it's still executable after conversion
+fi
 
-# Update packages and install utilities
-echo "UPDATING SYSTEM..."
-apt update && apt install -y curl git unzip nano default-mysql-client
-echo "UPDATE COMPLETE."
+# # Set PHP memory limit
+# echo "INCREASING PHP MEMORY LIMIT..."
+# echo "MEMORY_LIMIT = 512M" > /usr/local/etc/php/conf.d/memory-limit.ini
+
+# # Enable OPcache
+# echo "ENABLING OPCACHE..."
+# echo "opcache.enable=1" > /usr/local/etc/php/conf.d/opcache.ini
+# echo "opcache.memory_consumption=128" >> /usr/local/etc/php/conf.d/opcache.ini
+# echo "opcache.max_accelerated_files=4000" >> /usr/local/etc/php/conf.d/opcache.ini
 
 # Set correct permissions for the files and translations directories
 echo "🔒 SETTING PERMISSIONS FOR THE FILES AND TRANSLATIONS DIRECTORIES..."
@@ -14,8 +25,24 @@ chmod -R 775 /var/www/html/sites/default/files
 echo "🔒 Setting permissions for the translations directory..."
 chown -R www-data:www-data /var/www/html/sites/default/files/translations
 chmod -R 775 /var/www/html/sites/default/files/translations
-echo "🔒 PERMISSIONS SET"
 
+# Set permission for db custom.cnf file
+chmod 644 /etc/mysql/conf.d/custom.cnf
+
+echo "PERMISSIONS SET"
+echo "RESTART CONTAINER TO TAKE EFFECT"
+
+# echo "DISABLING KEEPALIVE FOR BETTER PERFORMANCE..."
+# echo "KeepAlive Off" >> /etc/apache2/apache2.conf
+
+# # Restart Apache for changes to take effect
+# echo "RESTARTING APACHE..."
+# service apache2 restart
+
+# Update packages and install utilities
+echo "UPDATING SYSTEM..."
+apt update && apt install -y curl git unzip nano default-mysql-client
+echo "UPDATE COMPLETE."
 
 echo "CHECKING IF COMPOSER IS INSTALLED"
 # Install Composer if not already installed
